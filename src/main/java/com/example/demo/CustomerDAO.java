097ac9475c27872cd57964abf4d0e087e44c4ca6
ravi9409 <39340361+ravi9409@users.noreply.github.com>
2022-04-29 01:10:35 +0530

    added s

--- src/main/java/com/example/demo/CustomerDAO.java
+++ src/main/java/com/example/demo/CustomerDAO.java
@@ -9,4 +9,5 @@ public interface CustomerDAO {
     public void addCustomer(Customer cust);
     public List<Customer> getAllCustomers();
     public List<Customer> getCustomersByCity(String city);
+    public List<Customer> getCustomersByCity(String citys);
 }
